666b5c4aa97ac3c2d0f3636474c979ea17f1295a
SOHAM KULKARNI <50440348+skk2106@users.noreply.github.com>
2021-04-15 13:32:14 +0530

    Project

--- DAY5/PASSWORD_GENERATOR.py
+++ DAY5/PASSWORD_GENERATOR.py
@@ -1,4 +1,3 @@
-#Password Generator Program
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -13,10 +12,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-nr_letter = random.sample(letters,nr_letters)
-nr_symbol = random.sample(symbols,nr_symbols)
-nr_number =  random.sample(numbers,nr_numbers)
-Password = nr_letter + nr_symbol + nr_number
-print(''.join(Password))
+
+password = ""
+for characters in range(1,nr_letters + 1):
+ password+= random.choice(letters)
+for characters in range(1,nr_symbols + 1):
+  password+= random.choice(symbols)
+for characters in range(1, nr_numbers + 1):
+  password+= random.choice(numbers)
+
+print(password)
